e32c37b5bc666933604bdf5879bc4bdaa9fcba30
James Wobser <james.wobser@gmail.com>
2022-12-12 10:05:40 -0500

    D3p2 Working

--- 3/main2.cpp
+++ 3/main2.cpp
@@ -1,10 +1,12 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <unordered_set>
+#include <set>
 #include <algorithm>
 #include <string_view>
 #include "main.hpp"
+#include <vector>
+#include <array>
 
 std::ifstream input;
 std::string buf;
@@ -13,19 +15,57 @@ int main(int argc, char** argv){
     (void)argc; // Unused
     (void)argv; // Unused
     input.open("input");
-    input >> buf;
     int score{0};
     while(!input.eof()){
-        try {
-            check_len(buf);
+        std::array<std::string, 3> lines;
+        for(int i{0}; i < 3; ++i){
+            input >> buf;
+            lines[i] = buf;
         }
-        catch(const std::invalid_argument& e){
-            std::cout << e.what();
-            return 1;
+        /*
+        for(int i{0}; i < 3; ++i){
+            std::cout << std::hex << reinterpret_cast<void*>(lines[i].data()) << '\t';
         }
+        */
+        std::cout << '\n';
+
+        // left, middle, right, common
+        std::array<std::set<char>, 4> charsets;
+        for(int i{0}; i < 3; ++i){
+            for(auto c : lines[i]){
+                charsets[i].insert(c);
+            }
+        }
+        for(auto c : charsets[0]){
+            if( charsets[1].count(c) > 0 &&
+                charsets[2].count(c) > 0
+              ){ charsets[3].insert(c);}
+        }
+        /*
+        for (auto c : lines[0]){
+            items_in_common.insert(c);
+        }
+        for (int i {1}; i < 3; ++i){
+            for(auto c : lines[i]){
+                if(items_in_common.find(c) == items_in_common.end()){
+                    items_in_common.erase(c);
+                }
+            }
+        }
+        */
+
+        for(auto c : charsets[3]){
+            std::cout << c << " ";
+            score += score_priority(c);
+        }
+
+        std::cout << '\n';
+
+        /*
         auto dupes = find_duplicates(buf);
         std::for_each(dupes.begin(), dupes.end(), [&](const char c){score+= score_priority(c);});
         input >> buf;
+        */
     }
     std::cout << "Score: " << score << '\n';
     return 0;
