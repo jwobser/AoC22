481eb9a0755a344c9882293c9c0fb407fc7bf4c4
James Wobser <james.wobser@gmail.com>
2022-12-12 10:08:17 -0500

    Remove dead code, debug printing

--- 3/main2.cpp
+++ 3/main2.cpp
@@ -2,10 +2,7 @@
 #include <iostream>
 #include <string>
 #include <set>
-#include <algorithm>
-#include <string_view>
 #include "main.hpp"
-#include <vector>
 #include <array>
 
 std::ifstream input;
@@ -22,13 +19,6 @@ int main(int argc, char** argv){
             input >> buf;
             lines[i] = buf;
         }
-        /*
-        for(int i{0}; i < 3; ++i){
-            std::cout << std::hex << reinterpret_cast<void*>(lines[i].data()) << '\t';
-        }
-        */
-        std::cout << '\n';
-
         // left, middle, right, common
         std::array<std::set<char>, 4> charsets;
         for(int i{0}; i < 3; ++i){
@@ -41,35 +31,11 @@ int main(int argc, char** argv){
                 charsets[2].count(c) > 0
               ){ charsets[3].insert(c);}
         }
-        /*
-        for (auto c : lines[0]){
-            items_in_common.insert(c);
-        }
-        for (int i {1}; i < 3; ++i){
-            for(auto c : lines[i]){
-                if(items_in_common.find(c) == items_in_common.end()){
-                    items_in_common.erase(c);
-                }
-            }
-        }
-        */
 
         for(auto c : charsets[3]){
-            std::cout << c << " ";
             score += score_priority(c);
         }
-
-        std::cout << '\n';
-
-        /*
-        auto dupes = find_duplicates(buf);
-        std::for_each(dupes.begin(), dupes.end(), [&](const char c){score+= score_priority(c);});
-        input >> buf;
-        */
     }
     std::cout << "Score: " << score << '\n';
     return 0;
 }
-
-
-
