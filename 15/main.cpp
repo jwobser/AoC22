fc20fc02b9cc0ebc360e60ff717ea4f717c93ff4
James Wobser <james.wobser@gmail.com>
2022-12-17 13:20:57 -0500

    Troubleshooting

--- 15/main.cpp
+++ 15/main.cpp
@@ -50,9 +50,11 @@ int main(int argc, char** argv){
         spans.push_back(sensor.get_span_at_target(TARGET_ROW));
     }
 
+    /*
     for (const auto& span : spans){
-        // std::cout << span << '\n';
+        std::cout << span << '\n';
     }
+     */
 
     auto combined_spans = combine_exclusions(spans);
     for (const auto& span : combined_spans){
@@ -64,32 +66,45 @@ int main(int argc, char** argv){
 std::vector<ExclusionSpan> combine_exclusions(std::vector<ExclusionSpan> input){
     std::vector<ExclusionSpan> combined;
     combined.push_back(input[0]);
+    for (const auto& span : combined){
+        std::cout << span << '\n';
+    }
     for(auto span : input){
         // Check if there are any overlaps.
         // FIXME Never executes because combined is empty
+        // TODO Refactor loop to deal with iterator invalidation.
+        // TODO Break out into separate file
+        // TODO Unitttest
         for(auto& cspan : combined){
             // no overlap
             if((span.end < cspan.start) || (span.start > cspan.end)){
+//                std::cout << "No overlap \n";
                 combined.push_back(span);
                 continue;
             }
             // complete overlap
             if((cspan.start <= span.start) && (cspan.end >= span.end)){
+//                std::cout << "Complete overlap \n";
                 continue;
             }
             // partial overlaps
             // Target span expands combined span to left
             if((span.start < cspan.start) && (span.end >= cspan.start) && (span.end <= cspan.end)){
+//                std::cout << "Expand Left\n";
                cspan.start = span.start;
             }
             // Target span expands combined span to the right
             if((span.end > cspan.end) && (span.start >= cspan.start) && (span.start <= cspan.end)){
+//                std::cout << "Expand Right\n";
                 cspan.end = span.end;
             }
         }
 
         // Finally, recheck all ranges for any overlap
 
+    }
+    for (const auto& span : combined){
+        std::cout << span << '\n';
     }
     return combined;
 }
