a5cc748e03531edb1b638f6f39d0b21962d90090
James Wobser <james.wobser@gmail.com>
2023-01-10 11:39:41 -0500

    Cleanup Unused functions and comments

--- java/src/main/kotlin/com/jimwobser/D25.kt
+++ java/src/main/kotlin/com/jimwobser/D25.kt
@@ -2,22 +2,11 @@ package com.jimwobser
 
 import java.io.BufferedReader
 import java.io.FileReader
-import java.math.BigInteger
-import kotlin.math.ceil
-import kotlin.math.floor
-import kotlin.math.ln
-import kotlin.math.pow
 
-fun main(args: Array<String>){
-    println("hello world")
+fun main(args: Array<String>) {
     val inputs = ArrayList<String>()
     ingest("d25.input", inputs)
-    for(s in inputs){
-        println("$s \t : ${parseLine(s)}")
-    }
-    val inputarrays = inputs.map { parseLine(it) }
-    val output = inputarrays.reduce { a, b -> addIntArray(a, b) }
-    println(output)
+    val output = inputs.map { parseLine(it) }.reduce { a, b -> addIntArray(a, b) }
     for(i in 0 until output.size - 1){
         while(output[i] > 2){
             output[i+1] += 1
@@ -28,7 +17,6 @@ fun main(args: Array<String>){
             output[i] += 5
         }
     }
-    println(output)
     val sb = StringBuilder()
     for(v in output.reversed()){
         sb.append(
@@ -40,76 +28,17 @@ fun main(args: Array<String>){
         )
     }
     println(sb.toString())
-//    println(inputarrays.reduce{a,b -> addIntArray(a,b)})
-    /*
-    println(inputs)
-    println(inputs.map{ parseLine(it)})
-    // Overflows a signed 32-bit int, so have to convert to unsigned.
-    val total = inputs.map({parseLine(it).toUInt()}).sum()
-    println("Sum: $total")
-    val totalBase5 = convertToBaseFive(total)
-    val offsetBase5 = offsetBaseFive(totalBase5)
-    println(convertToSNAFU(offsetBase5))
-//    inputs.forEach{println(it)}
-//    val snafuSum: BigInteger = inputs.map({parseLine(it).toBigInteger()}).sumOf { it }
-//    println("Snafu Sum: $snafuSum")
-
-     */
-
 }
 
-// Java overload for UInt type
-fun convertToBaseFive(n: Int): ArrayList<Int>{
-    return convertToBaseFive(n.toUInt())
-}
-
-fun convertToBaseFive(n: UInt): ArrayList<Int>{
-    var w = n
-    val order = (ceil(ln(n.toDouble()) / ln(5.0)) ).toInt()
-    var result = MutableList(order + 1){0} as ArrayList<Int>
-    for(i in 1 .. order){
-        val p = 5.0.pow(order - i).toUInt()
-        result[i] = (w / p).toInt()
-        w -= result[i].toUInt() * p
-    }
-    return result
-}
-
-fun offsetBaseFive(v: ArrayList<Int>): ArrayList<Int>{
-    while(v.any{it > 2}) {
-        for (i in 1 until v.size) {
-            if (v[i] > 2) {
-                v[i - 1] += 1
-                v[i] -= 5
-            }
-        }
-    }
-    return v
-}
-
-
-fun convertToSNAFU(v: ArrayList<Int>): String{
-    var result = StringBuilder()
-    for(i in v){
-        result.append(
-        when(i){
-            -2 -> '='
-            -1 -> '-'
-            else -> i
-        } )
-    }
-    return result.toString()
-}
-
-inline fun addIntArray(a: ArrayList<Int>, b: ArrayList<Int>): ArrayList<Int> {
-    var result = ArrayList<Int>()
+fun addIntArray(a: ArrayList<Int>, b: ArrayList<Int>): ArrayList<Int> {
+    val result = ArrayList<Int>()
     if( a.size > b.size) {
         a.forEach { result.add(it) }
         for(i in 0 until b.size){
             result[i] += b[i]
         }
     } else {
-        b.forEach(){result.add(it)}
+        b.forEach {result.add(it)}
         for(i in 0 until a.size){
             result[i] += a[i]
         }
@@ -119,7 +48,7 @@ inline fun addIntArray(a: ArrayList<Int>, b: ArrayList<Int>): ArrayList<Int> {
 fun parseLine(line: String): ArrayList<Int> {
     val result = ArrayList<Int>()
     for (i in line.length - 1 downTo 0) {
-        val c = line.get(i)
+        val c = line[i]
         result.add(
             when (c) {
                 '=' -> -2
@@ -139,6 +68,4 @@ fun ingest(name: String, dest: ArrayList<String> ){
     file.close()
 }
 
-class D25 {
-
-}
+class D25
--- java/src/test/java/D25Test.java
+++ java/src/test/java/D25Test.java
@@ -9,63 +9,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 public class D25Test {
-    /*
-    @Test
-    public void parseLineExample(){
-        Integer result = D25Kt.parseLine("1=-0-2");
-        assertEquals(result, 1747);
-        result = D25Kt.parseLine("12111");
-        assertEquals(result, 906);
-        result = D25Kt.parseLine("2=0=");
-        assertEquals(result, 198);
-        result = D25Kt.parseLine("1=-1=");
-        assertEquals(result, 353);
-    }
-
-    @Test
-    public void convertToBaseFive(){
-        var result = D25Kt.convertToBaseFive(Integer.valueOf(30));
-        var listexpected = List.of(0,1,1,0);
-        var expected = new ArrayList<Integer>(listexpected);
-        assertEquals(expected, result);
-        result = D25Kt.convertToBaseFive(Integer.valueOf(26));
-        expected = new ArrayList<Integer>(List.of(0,1,0,1));
-        assertEquals(expected, result);
-        result = D25Kt.convertToBaseFive(Integer.valueOf(31));
-        expected = new ArrayList<Integer>(List.of(0,1,1,1));
-        assertEquals(expected, result);
-        result = D25Kt.convertToBaseFive(Integer.valueOf(125));
-        expected = new ArrayList<Integer>(List.of(0,1,0,0,0));
-        assertEquals(expected, result);
-        result = D25Kt.convertToBaseFive(Integer.valueOf(3124));
-        expected = new ArrayList<Integer>(List.of(0,4,4,4,4,4));
-        assertEquals(expected, result);
-        result = D25Kt.convertToBaseFive(Integer.valueOf(80));
-        expected = new ArrayList<Integer>(List.of(0,3,1,0));
-        assertEquals(expected, result);
-    }
-
-    @Test
-    public void changeOffset(){
-        var result = D25Kt.offsetBaseFive(new ArrayList<Integer>(List.of(0,3,1,0)));
-        var expected = new ArrayList<Integer>(List.of(1,-2,1,0));
-        assertEquals(expected, result);
-    }
-
-    @Test
-    public void changeOffset2(){
-//        System.out.println(D25Kt.convertToBaseFive(Integer.valueOf(353)).toString());
-        var result = D25Kt.offsetBaseFive(new ArrayList<>(List.of(0,2,4,0,3)));
-        var expected = new ArrayList<>(List.of(1,-2,-1,1,-2));
-        assertEquals(expected, result);
-        result = D25Kt.offsetBaseFive(D25Kt.convertToBaseFive(Integer.valueOf(1747)));
-        expected = new ArrayList<>(List.of(1,-2,-1,0,-1, 2));
-        assertEquals(expected, result);
-//        System.out.println(result);
-    }
-
-    */
-
     @Test
     public void addArrays(){
         var expected = new ArrayList<>(List.of(0,1,1,10));
@@ -78,5 +21,4 @@ public class D25Test {
         b = new ArrayList<>(List.of(0,9,9,9,9));
         assertEquals(expected, D25Kt.addIntArray(a,b));
     }
-
 }
